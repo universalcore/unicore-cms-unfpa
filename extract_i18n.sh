ea451e890cbfb04ed543967e46300cfaf507964a
Simon de Haan <simon@praekeltfoundation.org>
2014-12-17 08:05:45 +0200

    update locale & add spanish for Colombia

--- extract_i18n.sh
+++ extract_i18n.sh
@@ -4,7 +4,7 @@ mkdir -p unicorecmsgem/locale
 
 pot-create -o unicorecmsgem/locale/unicorecmsgem.pot unicorecmsgem/
 
-declare -a arr=("eng_GB" "tha_TH" "ind_ID" "swa_TZ" "hin_IN" "fre_FR")
+declare -a arr=("eng_GB" "tha_TH" "ind_ID" "swa_TZ" "hin_IN" "fre_FR" "spa_CO")
 
 for lang in "${arr[@]}"
 do
