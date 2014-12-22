a08c25dbaf646cb286c289aca169fbbee14974cd
Simon de Haan <simon@praekeltfoundation.org>
2014-12-22 13:34:29 +0200

    update MX & CO

--- extract_i18n.sh
+++ extract_i18n.sh
@@ -4,7 +4,7 @@ mkdir -p unicorecmsgem/locale
 
 pot-create -o unicorecmsgem/locale/unicorecmsgem.pot unicorecmsgem/
 
-declare -a arr=("eng_GB" "tha_TH" "ind_ID" "swa_TZ" "hin_IN" "fre_FR" "spa_CO")
+declare -a arr=("eng_GB" "tha_TH" "ind_ID" "swa_TZ" "hin_IN" "fre_FR" "spa_CO" "spa_MX")
 
 for lang in "${arr[@]}"
 do
